1c9f7e94340bcaa00109480e36ecbaef1b76bec8
Felipe <feload@gmail.com>
2018-06-15 16:27:05 -0500

    Initial commit

---
+++ client.js
@@ -0,0 +1,35 @@
+const {ChatManager, TokenProvider} = require('@pusher/chatkit');
+const {JSDOM} = require('jsdom');
+const util = require('util');
+const prompt = require('prompt');
+
+const makeChatkitNodeCompatible = () => {
+  const {window} = new JSDOM();
+  global.window = window;
+  global.navigator = {};
+};
+
+makeChatkitNodeCompatible();
+
+const main = async () => {
+  try {
+    prompt.start();
+    prompt.message = '';
+
+    const get = util.promisify(prompt.get);
+
+    const usernameSchema = [
+      {
+        description: 'Enter your name',
+        name: 'username',
+        required: true
+      }
+    ];
+
+    const { username } = await get(usernameSchema);
+  } catch (err) {
+    console.log(`Failed with ${err}`);
+    process.exit(1);
+  }
+}
+main();
